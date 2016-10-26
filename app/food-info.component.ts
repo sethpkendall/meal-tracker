7249d26169ac2cf24d5a080e151a40e60670bd5e
Seth Kendall <sethpkendall@gmail.com>
2016-10-25 23:27:41 -0700

    save edit page with two way binding set up but non-functional

--- app/food-info.component.ts
+++ app/food-info.component.ts
@@ -1,12 +1,19 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Food } from './food.model';
 
 @Component({
   selector: "food-info",
   template:`
     <div *ngIf="clickedFood">
-      <h2>{{}}</h2>
+      <div class='form-group'>
+        <input class='form-control' type='text' [(ngModel)]="clickedFood.name">
+        <input class='form-control' type='number' [(ngModel)]="clickedFood.calories">
+        <input class='form-control' type='text' [(ngModel)]="clickedFood.details">
+      </div>
+    </div>
   `
 })
 
-export class FoodInfoComponent
+export class FoodInfoComponent{
+  @Input() clickedFood:Food;
+}
