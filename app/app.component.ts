4c065fc54cf5b040ba9738ba46fd87125d1353b6
Seth Kendall <sethpkendall@gmail.com>
2016-10-25 20:59:42 -0700

    add food model

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import { Food } from './food.model';
 
 @Component({
   selector:'my-app',
   template: `
   <div class="container">
-    
-  </div>
+    <h1>Meal Tracker</h1>
+    <div class='row'>
+      <div class='col-xs-6'>
+      <food-list [testFoods]></food-list>
+      </div>
+    </div>
   `
 })
 
 export class AppComponent{
-
+  public testFoods: food[] = [
+    new Food('Fries', 500, "Nice and greasy"),
+    new Food('Carrots with Ranch', 200, "Healthy and boring with a compromise"),
+    new Food('Southwest Salad', 400, "Lettuce, black beans, tortilla strips, corn and cilantro dressing with chicken on top (grilled)")
+  ];
 }
